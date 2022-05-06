b32e7421bfcc17b9b842301a00e0c6c801e24270
Prakhar <prakhar@everything.net.in>
2022-05-06 20:24:41 +0530

    fix lint
    Signed-off-by: Prakhar <prakhar@everything.net.in>

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Card.module.css";
 import { BiLinkExternal } from "react-icons/bi";
+import Image from "next/image";
 
 const Card = ({ img, heading, desc, link }) => {
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <div class={`${styles.animatedbox} ${styles.in}`}>
-        <img src={img} alt="image" />
+      <div className={`${styles.animatedbox} ${styles.in}`}>
+        <Image src={img} alt="image" height={100} width={100} />
         <h1>{heading}</h1>
         <p>{desc}</p>
         <BiLinkExternal className={styles.link} />
